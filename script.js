418eab34f25cd097115be4d6e003fd37251a126a
Stephen Howard <Showard1793@gmail.com>
2024-08-13 12:42:09 -0500

    Update

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 
 const { formSubmission } = require("./scriptHelper");
     
-    //SUBMIT FORM
-   this.window.addEventListener("submit", formSubmission())
+//TESTING OUT NEW SUBMIT FORM
+ window.addEventListener("load", function() {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        // Get values from the form
+        let pilotName = document.querySelector("input[name='pilotName']").value;
+        let copilotName = document.querySelector("input[name='copilotName']").value;
+        let fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+        let cargoMass = document.querySelector("input[name='cargoMass']").value;
+
+        // Call formSubmission with these values
+        formSubmission(document, pilotName, copilotName, fuelLevel, cargoMass);
+    });
+});     
+
+//OLD SUBMIT FORM
+   //this.window.addEventListener("submit", formSubmission())
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
